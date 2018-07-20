0600f14c74cfdd195ecfd9a6d396e11bfacaa635
artifishional <artifishional@mail.ru>
2018-07-20 11:24:15 +0300

    - fix typo

--- src/loader/script_like_promise.js
+++ src/loader/script_like_promise.js
@@ -25,12 +25,12 @@ export default function ({path}) {
             const xhr = new XMLHttpRequest();
             xhr.open("GET", path, true);
             xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-            xhr.setRequestHeader('Content-type', 'application/xml; charset=utf-8');
+            xhr.setRequestHeader('Content-type', 'text/html; charset=utf-8');
             xhr.onload = () => {
-                const doc = new DOMParser().parseFromString(xhr.responseText, "application/xml");
+                const doc = new DOMParser().parseFromString(xhr.responseText, "text/html");
                 const err = doc.querySelector("parsererror");
                 if(err) throw `${path} parser error: ${err.innerText}`;
-                const res = transform(doc.firstChild);
+                const res = transform(doc.querySelector("body").children[0]);
                 resolve( {module: {default: res}} );
             };
             xhr.send();
