e44063101e91570aa1200e9376715efc62dda875
Шелкоуденко Аким Германович <ag_shelkoudenko@bingo-boom.com>
2019-04-17 15:44:00 +0300

    a bit optimize

--- src/loader/inline-style.js
+++ src/loader/inline-style.js
@@ -3,16 +3,17 @@ import csstree from "css-tree";
 import FontFaceObserver from "fontfaceobserver";
 import ImagePreloader from "image-preloader";
 
+const FONT_LOADING_TIMEOUT = 30000;
+
 export default ({ style, path, ...args }) =>
   stream((emt, { sweep }) => {
-    const fontFaceStyle = document.createElement("style");
-    const otherStyle = document.createElement("style");
-    fontFaceStyle.type = otherStyle.type = "text/css";
-    fontFaceStyle.textContent = otherStyle.textContent = "";
+    const commonStyle = document.createElement("style");
+    commonStyle.type = "text/css";
+    commonStyle.textContent = "";
 
-    const observers = [];
-    const fonts = [];
-    const images = [];
+    const targets = [];
+    let rawFontCSSContent = "";
+    let rawCommonCSSContent = "";
 
     const ast = csstree.parse(style.textContent);
     ast.children.forEach(node => {
@@ -22,7 +23,7 @@ export default ({ style, path, ...args }) =>
           const { type, property, value } = prop;
           if (type === "Declaration" && property === "font-family") {
             value.children.forEach(e => {
-              fonts.push(e.value.replace(/"/g, ""));
+              targets.push({ raw: e.value.replace(/"/g, ""), type: "font" });
             });
           }
           if (type === "Declaration" && property === "src") {
@@ -34,7 +35,7 @@ export default ({ style, path, ...args }) =>
             });
           }
         });
-        fontFaceStyle.textContent += csstree.generate(node);
+        rawFontCSSContent += csstree.generate(node);
       } else if (type === "Rule") {
         block.children.forEach(prop => {
           const { property, value } = prop;
@@ -44,31 +45,40 @@ export default ({ style, path, ...args }) =>
               if (type === "Url") {
                 const v = "m2units/" + path + value.value.replace(/"/g, "");
                 value.value = v;
-                images.push(v);
+                targets.push({ raw: v, type: "img" });
               }
             });
           }
         });
-        otherStyle.textContent += csstree.generate(node);
+        rawCommonCSSContent += csstree.generate(node);
       } else {
-        otherStyle.textContent += csstree.generate(node);
+        rawCommonCSSContent += csstree.generate(node);
       }
     });
-    if (fontFaceStyle.textContent !== "") {
+
+    commonStyle.textContent = rawCommonCSSContent;
+    if (rawCommonCSSContent) {
+      const fontFaceStyle = document.createElement("style");
+      fontFaceStyle.textContent = rawFontCSSContent;
+      fontFaceStyle.type = "text/css";
       document.head.appendChild(fontFaceStyle);
     }
 
-    images.forEach(img => {
-      observers.push(new ImagePreloader().preload(...images));
-    });
-    fonts.forEach(fontName => {
-      observers.push(new FontFaceObserver(fontName).load(null, 30000));
+    Promise.all(
+      targets.map(({ raw, type }) => {
+        if (type === "font") {
+          return new FontFaceObserver(raw).load(null, FONT_LOADING_TIMEOUT);
+        } else if (type === "img") {
+          return new ImagePreloader().preload(raw);
+        }
+      })
+    ).then(() => {
+      document.head.appendChild(commonStyle);
+      emt({ type: "inline-style", style: commonStyle, ...args });
     });
 
-    Promise.all(observers).then(() => {
-      document.head.appendChild(otherStyle);
-      emt({ type: "inline-style", style: otherStyle, ...args });
+    sweep.add(() => {
+      commonStyle.remove();
+      fontFaceStyle && fontFaceStyle.remove();
     });
-
-    sweep.add(() => style.remove());
   });
