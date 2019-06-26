dc998b54972da793571f3954d0377aa737177649
Гаврись Юлия Васильевна <yv_gavris@bbgroup.local>
2019-06-26 14:28:38 +0300

    whatever

--- src/loader/inline-style.js
+++ src/loader/inline-style.js
@@ -14,124 +14,186 @@ function FileReader(blob) {
 }
 
 function createPrioritySystemStyle(priority) {
-    while(PRIORITY.length < priority+1) {
-        const style = document.createElement("style");
-        PRIORITY.push(style);
-        style.setAttribute("data-priority", `${PRIORITY.length-1}`);
-        PRIORITY[PRIORITY.length-2].before(style);
-    }
+	while(PRIORITY.length < priority+1) {
+		const style = document.createElement("style");
+		PRIORITY.push(style);
+		style.setAttribute("data-priority", `${PRIORITY.length-1}`);
+		PRIORITY[PRIORITY.length-2].before(style);
+	}
 }
 
 function inject(style, priority) {
-    createPrioritySystemStyle(priority);
-    PRIORITY[priority].after(style);
+	createPrioritySystemStyle(priority);
+	PRIORITY[priority].after(style);
 }
 
 const PRIORITY = [];
 
 export default ({ acid, priority, style, path, revision, ...args }) => {
- 
+
 	return stream(async (emt, {sweep}) => {
-	    
-	    if(!PRIORITY[0]) {
-		    const zero = document.createElement("style");
-		    zero.setAttribute("data-priority", "0");
-		    document.head.append(zero);
-		    PRIORITY[0] = zero;
-        }
-	    
-	    style.textContent = style.textContent.replace(/:scope/g, `[data-scope-acid-${acid}]`);
-	    
+
+		if(!PRIORITY[0]) {
+			const zero = document.createElement("style");
+			zero.setAttribute("data-priority", "0");
+			document.head.append(zero);
+			PRIORITY[0] = zero;
+		}
+
+		style.textContent = style.textContent.replace(/:scope/g, `[data-scope-acid-${acid}]`);
+
 		let isActive = true;
 		let fontFaceStyle = null;
-		const commonStyle = document.createElement("style");
-		commonStyle.textContent = "";
-		
+
 		const targets = [];
 		let rawFontCSSContent = "";
 		let rawCommonCSSContent = "";
-		
+
 		const ast = csstree.parse(style.textContent);
-		for (const node of ast.children.toArray()) {
-			const {type, name, block} = node;
-			if (type === "Atrule" && name === "font-face") {
-				for (const prop of block.children.toArray()) {
-					const {type, property, value} = prop;
-					if (type === "Declaration" && property === "font-family") {
-						for (const e of value.children.toArray()) {
-							targets.push({raw: e.value.replace(/"/g, ""), type: "font"});
+
+		const images = [];
+
+		csstree.walk(ast, async function(node, item, list) {
+			if (node.type === 'Url') {
+				const value = node.value.value;
+				if (value.indexOf("data:image") === -1) {
+					let url = "m2units/" + path + value.replace(/"/g, "");
+					if (revision) {
+						if (url.indexOf('?') > -1) {
+							url = `${url}&rev=${revision}`
+						} else {
+							url = `${url}?rev=${revision}`
 						}
 					}
-					if (type === "Declaration" && property === "src") {
-						for (const e of value.children.toArray()) {
-							const {type, value} = e;
-							if (type === "Url" && value.value.indexOf("data:image") === -1) {
-								let url = "m2units/" + path + value.value.replace(/"/g, "");
-								if (revision) {
-									if (url.indexOf('?') > -1) {
-										url = `${url}&rev=${revision}`
-									} else {
-										url = `${url}?rev=${revision}`
-									}
-								}
-								e.value.value = "m2units/" + path + value.value.replace(/"/g, "");
-							}
-						}
-					}
+					images.push(url);
 				}
-				rawFontCSSContent += csstree.generate(node);
-			} else if (type === "Rule") {
-				for (const prop of block.children.toArray()) {
-					const {property, value} = prop;
-					if (property === "background-image" || property === "background") {
-						for (const e of value.children.toArray()) {
-							const {type, value} = e;
-							if (type === "Url" && value.value.indexOf("data:image") === -1) {
-								let url = "m2units/" + path + value.value.replace(/"/g, "");
-								if (revision) {
-									if (url.indexOf('?') > -1) {
-										url = `${url}&rev=${revision}`
-									} else {
-										url = `${url}?rev=${revision}`
-									}
-								}
-								await fetch(url)
-									.then(r => r.blob())
-									.then(FileReader)
-									.then(({target: {result: base64}}) => {
-										value.value = base64;
-									})
-							}
-						}
-					}
-				}
-				rawCommonCSSContent += csstree.generate(node);
-			} else {
-				rawCommonCSSContent += csstree.generate(node);
-			}
-		}
-		commonStyle.textContent = rawCommonCSSContent;
-		if (rawFontCSSContent) {
-			fontFaceStyle = document.createElement("style");
-			fontFaceStyle.textContent = rawFontCSSContent;
-			document.head.appendChild(fontFaceStyle);
-		}
-		
-		Promise.all(
-			targets.map(({raw, type}) => {
-				if (type === "font") {
-					return new FontFaceObserver(raw).load(null, FONT_LOADING_TIMEOUT);
-				} else if (type === "img") {
-					return new ImagePreloader().preload(raw);
-				}
-			})
-		).then(() => {
-			if (isActive) {
-			    inject(commonStyle, priority);
-				emt({type: "inline-style", style: commonStyle, ...args});
 			}
 		});
-		
+
+		const result = images.map(async (url) => {
+			const promise = new Promise((resolve, reject) => {
+				fetch(url)
+					.then(r => r.blob())
+					.then(FileReader)
+					.then(({target: {result: base64}}) => {
+						resolve({
+							[url]: base64
+						})
+					})
+			});
+			return promise;
+		});
+
+		Promise.all(result).then((data) => {
+			const images = data.reduce((acc, elem) => {
+				return {
+					...acc,
+					...elem
+				}
+			}, {});
+			csstree.walk(ast, async function(node, item, list) {
+				if (node.type === 'Url') {
+					const value = node.value.value;
+					node.value.value = images[value];
+					if (value.indexOf("data:image") === -1) {
+						let url = "m2units/" + path + value.replace(/"/g, "");
+						if (revision) {
+							if (url.indexOf('?') > -1) {
+								url = `${url}&rev=${revision}`
+							} else {
+								url = `${url}?rev=${revision}`
+							}
+						}
+						node.value.value = images[url];
+					}
+				} else if (this.atrule && this.atrule.name === "font-face") {
+					console.warn('whatever')
+				}
+			});
+
+			const result = csstree.generate(ast, { sourceMap: false });
+			const commonStyle = document.createElement("style");
+			commonStyle.append(result);
+
+			// Promise.all(
+			// 	targets.map(({raw, type}) => {
+			// 		if (type === "font") {
+			// 			return new FontFaceObserver(raw).load(null, FONT_LOADING_TIMEOUT);
+			// 		} else if (type === "img") {
+			// 			return new ImagePreloader().preload(raw);
+			// 		}
+			// 	})
+			// ).then(() => {
+			if (isActive) {
+				inject(commonStyle, priority);
+				emt({type: "inline-style", style: commonStyle, ...args});
+			}
+			// });
+		})
+
+		// console.warn([...result]);
+
+		// csstree.walk(ast, async function(node, item, list) {
+		// 	if (list) {
+		// 		if (this.rule && node.type === 'Url') {
+		// 			const value = node.value.value;
+		// 			if (value.indexOf("data:image") === -1) {
+		// 				let url = "m2units/" + path + value.replace(/"/g, "");
+		// 				if (revision) {
+		// 					if (url.indexOf('?') > -1) {
+		// 						url = `${url}&rev=${revision}`
+		// 					} else {
+		// 						url = `${url}?rev=${revision}`
+		// 					}
+		// 				}
+		// 				await fetch(url)
+		// 					.then(r => r.blob())
+		// 					.then(FileReader)
+		// 					.then(({target: {result: base64}}) => {
+		// 						node.value.value = base64;
+		// 					})
+		// 			}
+		// 		} else if (this.atrule && this.atrule.name && node.type === 'Url') {
+		// 			const value = node.value.value;
+		// 			if (value.indexOf("data:image") === -1) {
+		// 				let url = "m2units/" + path + value.replace(/"/g, "");
+		// 				if (revision) {
+		// 					if (url.indexOf('?') > -1) {
+		// 						url = `${url}&rev=${revision}`
+		// 					} else {
+		// 						url = `${url}?rev=${revision}`
+		// 					}
+		// 				}
+		// 				await fetch(url)
+		// 					.then(r => r.blob())
+		// 					.then(FileReader)
+		// 					.then(({target: {result: base64}}) => {
+		// 						node.value.value = base64;
+		// 					})
+		// 			}
+		// 		}
+		// 	}
+		// });
+
+		// const result = csstree.generate(ast, { sourceMap: false });
+		// const commonStyle = document.createElement("style");
+		// commonStyle.append(result);
+
+		// Promise.all(
+		// 	targets.map(({raw, type}) => {
+		// 		if (type === "font") {
+		// 			return new FontFaceObserver(raw).load(null, FONT_LOADING_TIMEOUT);
+		// 		} else if (type === "img") {
+		// 			return new ImagePreloader().preload(raw);
+		// 		}
+		// 	})
+		// ).then(() => {
+		// 	if (isActive) {
+		// 	    inject(commonStyle, priority);
+		// 		emt({type: "inline-style", style: commonStyle, ...args});
+		// 	}
+		// });
+
 		sweep.add(() => {
 			isActive = false;
 			commonStyle.remove();
