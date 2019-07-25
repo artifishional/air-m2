3a4ee25263fa41b21ba4c4534d2f49a214811d20
artifishional <artifishional@mail.ru>
2019-07-25 18:35:38 +0300

    - loader paths - updated

--- src/loader/html.js
+++ src/loader/html.js
@@ -1,16 +1,5 @@
-import stream from "./xhr"
-/*
-export default ({path, revision}) => stream({path, revision, content: { type: "text/html" }})
-    .map( xhr => {
-        const doc = new DOMParser().parseFromString(xhr.responseText, "text/html");
-        const err = doc.querySelector("parsererror");
-        if(err) throw `${path} parser error: ${err.innerText}`;
-        return { content: doc.querySelector("body").children[0] };
-    } );
-*/
-
 export default ({path, revision}) => {
-    const url = new URL(path, window.location.origin);
+    const url = new URL(path, window.location.origin + window.location.pathname);
     if (revision) {
         url.searchParams.append("revision", revision);
     }
