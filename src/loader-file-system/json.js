3f511c0703dbc1bc399397c71f0c6e6dd94e448f
Маков Андрей Вячеславович <av_makov@bingo-boom.com>
2019-10-07 18:44:51 +0300

    res loader refactor

--- src/loader-file-system/json.js
+++
@@ -1,9 +0,0 @@
-export default ({url: path, revision}) => {
-  const fs = require('fs');
-  let url = revision ? `${path}?rev=${revision}` : path;
-  return new Promise(resolve => {
-    fs.readFile(url, (err, data) => {
-      resolve({ content: JSON.parse(data.toString()) });
-    });
-  });
-};
