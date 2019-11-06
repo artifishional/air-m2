e4f660ba32df43847a78a57d3b124c84f8ecb751
Маков Андрей Вячеславович <av_makov@bingo-boom.com>
2019-11-06 19:07:38 +0300

    fix globals

--- src/globals.js
+++ src/globals.js
@@ -1,4 +1,4 @@
 export const VIEW_PLUGINS = new Map();
-export const REVISION = document.currentScript && document.currentScript.getAttribute('revision') || '';
-export const PORT = window.port;
+export const REVISION = document.currentScript.getAttribute('revision') || '';
+export const PORT = document.currentScript.getAttribute('port') || '';
 export const ENTRY_UNIT = document.currentScript.getAttribute("data-entry-unit") || "master";
