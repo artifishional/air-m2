4f01a47a422bfc424421bed553bfa8ec08029e03
artifishional <artifishional@mail.ru>
2022-05-13 15:41:45 +0300

    Version update

--- src/loader/loader.mjs
+++ src/loader/loader.mjs
@@ -1,52 +1,48 @@
 const schtypes = {
-    "js": "/index.mjs",
-    "json": "/index.json",
-    "html": "/index.html",
+  js: '/index.js',
+  json: '/index.json',
+  html: '/index.html',
 };
 
 export default class Loader {
+  constructor({ path = 'm2units/' } = {}) {
+    this.rpath = path;
+    this.modules = [];
+  }
 
-    constructor({path = "m2units/"} = {}) {
-        this.rpath = path;
-        this.modules = [];
+  obtain({ path: _path, schtype = 'js' }, resourceloader) {
+    if (!_path) throw new Error("'path' param is required");
+    const path = (_path + schtypes[schtype]).replace(/\.\//g, '');
+    const exist = this.modules.find(({ path: x }) => path === x);
+    if (exist) {
+      return exist.module;
     }
 
-    obtain( { path: _path, schtype = "js" }, resourceloader ) {
-        if(!_path) throw "'path' param is required";
-        const path = (_path + schtypes[schtype]).replace(/\.\//g, '');
-        const exist = this.modules.find( ({ path: _path }) => path === _path );
-        if(exist) {
-            return exist.module;
-        }
-        else {
-            let module = null;
-            if (schtype === "html") {
-                module = resourceloader(resourceloader, {path: _path + '/'}, {url: schtypes[schtype], type: 'html'})
-                  .then( html => ({ data: html.content, pack: { cache: {}, path: _path + "/" } }) );
-            }
-            else {
-                module =
-                    /*todo es6 dynamic
-                    eval(`import("./${this.rpath}${path}")`).then(module => {
-                        emt({data: module});
-                    } );
-                    */
-                    resourceloader(resourceloader, {path: _path + '/'}, {url: schtypes[schtype], type: 'script'}).then(({module}) => {
-                        return { data: module, pack: { cache: {}, path: _path + "/" } };
-                    } );
-
-                    /*
-                    include({path: `${this.rpath}${path}`, revision}).then(({module}) => {
-                        emt( { data: module, pack: { path: _path + "/" } } );
-                    } );*/
-            }
-            this.modules.push({module, path});
-            return module;
-        }
+    let module = null;
+    if (schtype === 'html') {
+      module = resourceloader(resourceloader, { path: `${_path}/` }, { url: schtypes[schtype], type: 'html' })
+        .then((html) => ({ data: html.content, pack: { cache: {}, path: `${_path}/` } }));
+    } else {
+    /* todo es6 dynamic
+    eval(`import("./${this.rpath}${path}")`).then(module => {
+        emt({data: module});
+    } );
+    */
+      module = resourceloader(resourceloader,
+        { path: `${_path}/` },
+        { url: schtypes[schtype], type: 'script' },
+      )
+        .then(({ module: data }) => ({ data, pack: { cache: {}, path: `${_path}/` } }));
+    /*
+    include({path: `${this.rpath}${path}`, revision}).then(({module}) => {
+        emt( { data: module, pack: { path: _path + "/" } } );
+    } ); */
     }
+    this.modules.push({ module, path });
+    return module;
+  }
 
-    //static default = new Loader();
-
+  // static default = new Loader();
 }
 
 Loader.default = new Loader();
